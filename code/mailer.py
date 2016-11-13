ab05c0e3389e2e3bbd1b8ddb3e65f6531e391aca
mmwebster <milo.webster@gmail.com>
2016-11-13 14:04:15 -0800

    added si7021 wiring diagram raw, fixed mailer for multiple msg contents, corrected lab manual typo

--- code/mailer.py
+++ code/mailer.py
@@ -17,11 +17,6 @@ class Mailer:
         self.from_pass = from_pass
         self.to_address = to_address
 
-        # format message
-        self.msg = MIMEMultipart()
-        self.msg['From'] = from_address
-        self.msg['To'] = to_address
-
         # Connect to the email server
         self.server = smtplib.SMTP('smtp.gmail.com', 587) # hostname, port
         self.server.starttls()
@@ -30,20 +25,23 @@ class Mailer:
         self.server.login(from_address, from_pass)
 
     def send(self, subject, body):
+        # format message w/ defaults
+        msg = MIMEMultipart()
+        msg['From'] = self.from_address
+        msg['To'] = self.to_address
         # define subject and body
-        self.msg['Subject'] = subject
-        self.msg.attach(MIMEText(body, 'plain'))
+        msg['Subject'] = subject
+        msg.attach(MIMEText(body, 'plain'))
 
         # format and send the email
-        text = self.msg.as_string() # package the email into a single string
+        text = msg.as_string() # package the email into a single string
         self.server.sendmail(self.from_address, self.to_address, text)
 
         print("Mailer: Sent email to " + self.to_address)
 
     # @desc Sends a test email
     def test(self):
-        self.send("RPi Test", "Mailer class is...")
-        self.send("RPi Test", "...functioning properly")
+        self.send("RPi Test", "Mailer class is functioning properly")
 
     def __exit__(self, exc_type, exc, traceback):
         print("Mailer: Exiting, cleaning up")
