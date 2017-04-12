8a1a9ea479b4ad7d42180da92c301c33797f597c
mmwebster <milo.webster@gmail.com>
2017-04-11 19:56:17 -0700

    Completed secrets file

--- lab-manual/example-secrets-tex.py
+++ lab-manual/example-secrets-tex.py
@@ -5,7 +5,7 @@
 #######################################################################
 class Secret:
     def __init__(self):
-        self.secrets = {"email_password": "xxxxxxxxx"}
+        self.secrets = {"email_password": "xxxxxxxxx", "email_from": "your.new.email@gmail.com", "email_to": "your.existing.email@gmail.com"}
 
     def fetch(self, key):
         if key in self.secrets.keys():
